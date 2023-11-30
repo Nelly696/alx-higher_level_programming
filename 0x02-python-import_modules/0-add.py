ef84dfeeb70ba02b49875df04c331d3f0aeb5ba5
Nelly696 <amarachinaomi819@gmail.com>
2023-11-30 09:36:46 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-if _ _name_ _ == "_ _main_ _":
-    """print the addition of 1 and 2"""
-    from add_ 0 import add
+if __name__ == "__main__":
+    """Print the addition of 1 and 2."""
+    from add_0 import add
 
     a = 1
     b = 2
-    print ("{} + {} = {}". format (a, b, add (a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-if _ _name_ _ == "_ _main_ _":
-    """print the number of and list of arguments."""
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
 
-    number = len(sys-argv) -1
+    number = len(sys.argv) - 1
     if number == 0:
         print("0 arguments.")
     elif number == 1:
         print("1 argument:")
     else:
-        print("{} arguments: ".format(number))
-        for i in range(number)
+        print("{} arguments:".format(number))
+    for i in range(number):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
