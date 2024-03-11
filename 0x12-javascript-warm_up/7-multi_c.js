2d60bbb0f44ecef86d6999d1a00f31b9903b13bb
Nelly696 <amarachinaomi819@gmail.com>
2024-03-11 12:16:50 -0700

    added

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,9 @@
 // Write a script that prints x time "is fun"
 const x = parseInt(Number(process.argv[2]));
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,13 @@
 const square = parseInt(process.argv[2]);
 
 if (!isNaN(square)) {
-	if (square > 0) {
-		for (let s = 0; s < square; s++) {
-			console.log('X'.repeat(square));
-		}
-	} else {
-		console.log('size most be a positive number');
-	}
+  if (square > 0) {
+    for (let s = 0; s < square; s++) {
+      console.log('X'.repeat(square));
+    }
+  } else {
+    console.log('size most be a positive number');
+  }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
